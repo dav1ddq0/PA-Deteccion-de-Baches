f89e6aa040de15956a687b45a93c4f3e9ca7cf41
David De Quesada <dquesda969x@gmail.com>
2022-05-28 16:23:02 -0400

    :.

--- lib/src/pages/scaler.dart
+++ lib/src/pages/scaler.dart
@@ -4,28 +4,26 @@ class SizeConfig {
   late MediaQueryData _mediaQueryData;
   late double screenWidth;
   late double screenHeight;
-  double blockSizeHorizontal;
-  double blockSizeVertical;
+  late double blockSizeHorizontal;
+  late double blockSizeVertical;
 
-  double _safeAreaHorizontal;
-  double _safeAreaVertical;
-  double safeBlockHorizontal;
-  double safeBlockVertical;
+  late double _safeAreaHorizontal;
+  late double _safeAreaVertical;
+  late double safeBlockHorizontal;
+  late double safeBlockVertical;
 
   SizeConfig(BuildContext context) {
-	_mediaQueryData = MediaQuery.of(context);
-	screenWidth = _mediaQueryData.size.width;
-	screenHeight = _mediaQueryData.size.height;
-	blockSizeHorizontal = screenWidth / 100;
-	blockSizeVertical = screenHeight / 100;
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    blockSizeHorizontal = screenWidth / 100;
+    blockSizeVertical = screenHeight / 100;
 
-	_safeAreaHorizontal = _mediaQueryData.padding.left + 
-	_mediaQueryData.padding.right;
-	_safeAreaVertical = _mediaQueryData.padding.top +
-	_mediaQueryData.padding.bottom;
-	safeBlockHorizontal = (screenWidth -
-	_safeAreaHorizontal) / 100;
-	safeBlockVertical = (screenHeight -
-	_safeAreaVertical) / 100;
+    _safeAreaHorizontal =
+        _mediaQueryData.padding.left + _mediaQueryData.padding.right;
+    _safeAreaVertical =
+        _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
+    safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
+    safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
   }
 }
